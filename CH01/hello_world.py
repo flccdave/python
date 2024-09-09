74ac130776951874e8f61bc8eaf4b7cbbc8593c4
flccdave <157420500+flccdave@users.noreply.github.com>
2024-09-09 18:45:22 +0000

    Added Jupyter Notebooks

--- CH01/hello_world.py
+++ CH01/hello_world.py
@@ -1,1 +1,17 @@
+# AUTHOR: Dave Ghidiu
+# CREATED: 2024-09-04
+# DESCRIPTION: Prints "Hello, World!" on the screen
+
+
+print(55 / 6)    # 9.166666666666666
+
+# The following complicated code will output text on the screen
 print("Hello, World!")
+
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
+# print("GAME OVER MAN!!!!")
