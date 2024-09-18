c956ef9e1374d46abba2aa0ad23f5323a4c6d96f
flccdave <157420500+flccdave@users.noreply.github.com>
2024-09-18 18:02:06 +0000

    moar notebooks

--- CH02/input_demo.py
+++ CH02/input_demo.py
@@ -1,14 +1,67 @@
-# number_three = int('6348957234987l43534534')
-number_three = int('6348957234987l43534534')
-# The value of `number_three` is 6
 
-# first_name = input("What is your name? ")
-# print(f"Nice to meet you, {first_name}!!!")
 
-# print()
+# user_input = int(input('Enter a number: '))
+# remainder = user_input % 2
 
-# age = int(input(f"How old are you, {first_name}? "))
-# print(f"You are {age} years old.")
+# if remainder == 0:
+#     print('EVEN')
+# else:
+#     print('ODD')
 
+
+
+
+
+
+
+
+
+
+
+# age = int(input('Please enter your age: '))
+# print(age)
 # next_year = age + 1
-# print(f"Next year you will be {next_year} years old.")
+# # next_year = int(age) + 1 // age is still an str
+# print(type(age))
+# print(f'Next year you will be {next_year} years old!')
+
+
+
+
+
+
+
+
+# # name = 'Dave'
+# # print(name * (3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # number_three = int('6348957234987l43534534')
+# # number_three = int('6348957234987l43534534')
+# # The value of `number_three` is 6
+
+# # first_name = input("What is your name? ")
+# # print(f"Nice to meet you, {first_name}!!!")
+
+# # print()
+
+# # age = int(input(f"How old are you, {first_name}? "))
+# # print(f"You are {age} years old.")
+
+# # next_year = age + 1
+# # print(f"Next year you will be {next_year} years old.")
