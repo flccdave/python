23b5dd7e19abc0c80322c15338ee715e74d58e43
flccdave <157420500+flccdave@users.noreply.github.com>
2024-12-02 18:50:07 +0000

    classes and dictionaries

--- OTHER/dictionary_classes.py
+++ OTHER/dictionary_classes.py
@@ -4,17 +4,17 @@ words = {
   "desk": "Furniture for studying or working. One flat surface."
 }
 
-warhammer.brand
+# warhammer.brand
 
 
 class Warrior:
   def __init__(self, name, age, weight, rank, weapon):
     self.name = name
     self.age = age
-	self.weight = weight
-	self.rank = rank
-	self.weapon = weapon
-	self.health = 100
+    self.weight = weight
+    self.rank = rank
+    self.weapon = weapon
+    self.health = 100
 
 w1 = Warrior("John", 36, 100, "Lt.", "battle axe")
 w2 = Warrior("Dave", 46, 160, "Captain", "bastard sword")
